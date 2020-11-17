1727e0b3ca82b4ba2710ceaab39a49a5c88b076a
Berishev Ravil <quadraticteam@gmail.com>
2020-11-17 17:33:11 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,46 +1,17 @@
-# @berish/emitter
+# @berish/orm-rethinkdb-file-adapter
 
-The library allows you to conveniently work with events. Supports standard methods of working with events, also has additional functionality for working with cached requests
+Адаптер файловой базы данных RethinkDB для @berish/orm
 
 ## Installation
 
 ```
-$ npm install @berish/emitter --save
+$ npm install @berish/orm-rethinkdb-file-adapter --save
 ```
 
 or
 
 ```
-$ yarn add @berish/emitter
+$ yarn add @berish/orm-rethinkdb-file-adapter
 ```
 
 **Supports typescript**
-
-## Interfaces
-
-```typescript
-type SubscribeType<Data> = (data: Data) => void | Promise<void>;
-type EventNameType = string | number;
-type EventMapBaseType = {
-  [eventName: string]: any;
-  [eventName: number]: any;
-};
-
-interface IEventObject<Data> {
-  eventName: EventNameType;
-  eventHash: string;
-  callback: SubscribeType<Data>;
-}
-```
-
-## Initialize
-
-Syntax: `class EventEmitter<EventMap extends EventMapBaseType>`
-
-Example:
-
-```typesciprt
-import EventEmitter from '@berish/emitter';
-
-const emitter = new EventEmitter();
-```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,71 @@
-import { EventEmitter } from './emitter';
+import { BaseFileAdapter, IBaseFileItem } from '@berish/orm';
+import * as r from 'rethinkdb';
 
-export * from './emitter';
-export default EventEmitter;
+export interface IRethinkDBAdapterParams {
+  host: string;
+  port: number;
+  dbName: string;
+  tableName: string;
+}
+
+export default class RethinkFileAdapter extends BaseFileAdapter<IRethinkDBAdapterParams> {
+  private connection: r.Connection = null;
+
+  private tables: string[] = [];
+  private tablesInWait: { [tableName: string]: Promise<void> } = {};
+
+  public async initialize(params: IRethinkDBAdapterParams) {
+    this.params = params;
+    this.connection = await r.connect({ db: params.dbName, host: params.host, port: params.port });
+    const dbList = await r.dbList().run(this.connection);
+    if (!dbList.includes(params.dbName)) {
+      await r.dbCreate(params.dbName).run(this.connection);
+    }
+  }
+
+  public async get(ids: string[], fetchData: boolean) {
+    const table = await this.table(this.params.tableName);
+    let seq = table.getAll(...ids);
+    if (!fetchData) seq = seq.without('data');
+    const cursor = await seq.run(this.connection);
+    return cursor.toArray<IBaseFileItem>();
+  }
+
+  public async create(items: IBaseFileItem[]) {
+    const table = await this.table(this.params.tableName);
+    await table.insert(items, { conflict: 'replace' }).run(this.connection);
+  }
+
+  public async delete(ids: string[]) {
+    const table = await this.table(this.params.tableName);
+    await table
+      .getAll(...ids)
+      .delete()
+      .run(this.connection);
+  }
+
+  private async table(tableName: string): Promise<r.Table> {
+    const db = r.db(this.params.dbName);
+    if (this.tables.includes(tableName)) return db.table(tableName);
+    if (this.tablesInWait[tableName]) {
+      await this.tablesInWait[tableName];
+      return this.table(tableName);
+    }
+    let resolvePromise: () => void = null;
+    this.tablesInWait[tableName] = new Promise<void>(resolve => (resolvePromise = resolve));
+    const tableList = await db.tableList().run(this.connection);
+    if (!tableList.includes(tableName)) {
+      await db.tableCreate(tableName).run(this.connection);
+      await db
+        .table(tableName)
+        .wait()
+        .run(this.connection);
+    }
+    this.tables.push(tableName);
+
+    resolvePromise();
+    delete this.tablesInWait[tableName];
+
+    return db.table(tableName);
+  }
+}
